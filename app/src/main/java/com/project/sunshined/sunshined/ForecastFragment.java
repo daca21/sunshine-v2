018691bd34a26f8f035e410cfe5c0a2e084971d3
daca21 <dacaramel@gmail.com>
2015-05-25 15:23:39 +0200

    6.01 Using Services

--- app/src/main/java/com/project/sunshined/sunshined/ForecastFragment.java
+++ app/src/main/java/com/project/sunshined/sunshined/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.project.sunshined.sunshined;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.project.sunshined.sunshined.data.WeatherContract;
+import com.project.sunshined.sunshined.service.SunshineService;
 
 /**
  * Created by Dac Duy Nguyen on 2/20/15.
@@ -171,9 +173,13 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     private void updateWeather() {
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+//        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
+//        String location = Utility.getPreferredLocation(getActivity());
+//        weatherTask.execute(location);
+        Intent intent = new Intent(getActivity(), SunshineService.class);
+        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,
+                Utility.getPreferredLocation(getActivity()));
+        getActivity().startService(intent);
     }
 
     @Override
