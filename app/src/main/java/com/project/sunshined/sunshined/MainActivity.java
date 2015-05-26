4c92c6974b2f9eb61f63c1d55c01d2953df2e281
daca21 <dacaramel@gmail.com>
2015-05-26 17:16:01 +0200

    6.05 Scheduled Synchronization

--- app/src/main/java/com/project/sunshined/sunshined/MainActivity.java
+++ app/src/main/java/com/project/sunshined/sunshined/MainActivity.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.project.sunshined.sunshined.sync.SunshineSyncAdapter;
+
 
 public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback {
 
@@ -45,6 +47,7 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
         ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
+        SunshineSyncAdapter.initializeSyncAdapter(this);
     }
 
 
